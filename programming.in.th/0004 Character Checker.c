5502cdb5dd16a7b191cf67c942b9fa336ae9d7d0
Akari47 <101469414+Akari47@users.noreply.github.com>
2024-01-29 16:06:25 +0900

    add 0003-0005

---
+++ programming.in.th/0004 Character Checker.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+    char x[10000];
+    int i = 0, s = 0, c = 0;
+    x[0] = getchar();
+    while (('a' <= x[i] && x[i] <= 'z') || ('A' <= x[i] && x[i] <= 'Z')){
+        if('a' <= x[i] && x[i] <= 'z'){
+            s += 1;
+        }
+        else{
+            c += 1;
+        }
+        i++;
+        x[i] = getchar();
+    }
+    if(s != 0 && c == 0){
+        printf("All Small Letter");
+    }
+    if(c != 0 && s == 0){
+        printf("All Capital Letter");
+    }
+    if(c != 0 && s != 0){
+        printf("Mix");
+    }
+}
